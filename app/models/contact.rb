f52c6453112b8e3caaf96521c73bbf523ec14574
Luke Busby <labusby1@gmail.com>
2019-01-02 15:06:15 +0000

    Removed a redundant contact get from routes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
     validates :name, presence: true
     validates :email, presence: true
-    validates :email, confirmation: {case_sensitive: false}
+    #validates :email, confirmation: {case_sensitive: false}
     validates :comments, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'projects', to: 'pages#projects'
-  get 'contact', to: 'pages#contact'
+  # get 'contact', to: 'pages#contact'
   
   resources :contacts, only: :create
   get 'contact-luke', to: 'contacts#new', as: 'new_contact'
